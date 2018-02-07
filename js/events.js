bd806c624c9b4c4496eb6ca933b2c474630f8a6a
arye dov eidelman <aryedov.e@gmail.com>
2018-02-07 18:50:17 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,4 @@
 function getIt(){
-  console.log('a');
   $('p').on('click', function(){
   alert('Hey!');
   })
@@ -7,14 +6,12 @@ function getIt(){
 
 function frameIt(){
   $('img').on('load', function(){
-    console.log('a');
     $('img').addClass('tasty');
   })
 }
 
 function pressIt(){
   $('input[type="text"]').on('keydown', function(e){
-    console.log('a');
     if(e.which === 71){
       alert('Hey!')
     }
@@ -23,13 +20,11 @@ function pressIt(){
 
 function submitIt(){
   $('form').on('submit', function(){
-    console.log('a');
     alert('Your form is going to be submitted now.');
   })
 }
 
 $(document).ready(function(){
-  console.log('a');
   getIt();
   frameIt();
   pressIt();
