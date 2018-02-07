37dc74b462204f8e63f2027bfd3a97866e65cab6
arye dov eidelman <aryedov.e@gmail.com>
2018-02-07 18:19:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,6 +10,18 @@ function frameIt(){
   })
 }
 
+function getIt(){
+  $('p').on('click', function(){
+  alert('Hey!')
+  })
+}
+
+function getIt(){
+  $('p').on('click', function(){
+  alert('Hey!')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
